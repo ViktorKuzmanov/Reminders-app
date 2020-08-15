327121ebb697ad24088616c9f8616e77f5a1ad7d
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-15 14:43:31 +0200

    add callback route /auth/google/redirect in router

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -7,8 +7,30 @@ passport.use(
     {
       clientID: keys.google.clientId,
       clientSecret: keys.google.clientSecret,
-      callbackURL: "http://localhost:5000/redirect",
+      callbackURL: "http://localhost:5000/auth/google/redirect",
     },
-    (accessToken, refreshToken, profile, cb) => {}
+    // This callback function is fired when user is redirected to callback route
+    function (accessToken, refreshToken, profile, done) {
+      console.log("function is executed");
+    }
   )
 );
+
+// // check if user already exists in our database
+// User.findOne({ googleId: profile.id }).then((currentUser) => {
+//   if (currentUser) {
+//     // we already have the user in database
+//     done(null, currentUser);
+//   } else {
+//     // if we don't already have the user create it in our Database
+//     new User({
+//       username: profile.displayName,
+//       googleId: profile.id,
+//       thumbnail: profile._json.picture,
+//     })
+//       .save()
+//       .then((newUser) => {
+//         done(null, newUser);
+//       });
+//   }
+// });
--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -6,4 +6,13 @@ const router = express.Router();
 // /auth/google route - Present the use with consent screen (on google button click)
 router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
 
+router.get(
+  "/google/redirect",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.send("you are on /redirect route");
+  }
+);
+
 module.exports = router;
