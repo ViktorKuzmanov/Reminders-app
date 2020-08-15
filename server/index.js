fa63f61e2900df8b07f1916da07773e03c465b8e
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-15 02:39:30 +0200

    connect to MongoDB Atlas database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const path = require("path");
+const mongoose = require("mongoose");
 const express = require("express");
+const keys = require("../config/keys");
 const app = express(); // create express app
+
+mongoose.connect(
+  `mongodb+srv://admin-viktor:${keys.mongoDB.adminPassword}@clusterforpetprojects.7esmr.mongodb.net/usersRemindersApp?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 // add middlewares
 app.use(express.static(path.join(__dirname, "..", "build")));
