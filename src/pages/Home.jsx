160c375bd65542c037aa38777ec34be896590b02
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-24 09:28:38 +0200

    add state in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import Navbar from "../components/NavBar";
 
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+
   render() {
     return (
       <div>
