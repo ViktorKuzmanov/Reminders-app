dad112b751be9884be13ecb6baf1e0cafc26a231
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-29 10:51:18 +0200

    show isLoggedIn data from UserContext

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "../components/NavBar";
+import { UserContext } from "../context/UserContext";
 
 const Home = () => {
+  const [isLoggedIn, setIsLoggedIn] = useContext(UserContext);
   return (
     <div>
       <Navbar />
-      This is the Homepage
+      This is the Homepage and isLoggedIn = {isLoggedIn.toString()}
     </div>
   );
 };
+
+export default Home;
