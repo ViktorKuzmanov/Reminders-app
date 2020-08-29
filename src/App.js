3fbf320afc3849e21c33ba8793f40dd44a76cdb1
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-29 10:31:21 +0200

    wrap routes in UserProvider - add UserContext

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import { UserProvider } from "./context/UserContext";
 
 export default class App extends React.Component {
   render() {
     return (
       <Router>
-        <Route path="/" exact component={Home}></Route>
-        <Route path="/about" exact component={About}></Route>
+        <UserProvider>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/about" exact component={About}></Route>
+        </UserProvider>
       </Router>
     );
   }
