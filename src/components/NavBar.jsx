72497ee71ec6b97fe3a7579de1958c5ae48e71cf
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-30 17:50:23 +0200

    convert NavBar from class to funtional component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,108 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
 import { Link, withRouter } from "react-router-dom";
 import { Dropdown, DropdownButton, Modal } from "react-bootstrap";
 import { Tab, Tabs } from "react-bootstrap";
 
-class NavBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activetab: "",
-      showModal: false,
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.onHide = this.onHide.bind(this);
-  }
+const NavBar = (props) => {
+  const [activetab, setActivetab] = useState("");
+  const [showModal, setShowModal] = useState(false);
 
-  handleClick(event) {
+  const handleClick = (event) => {
     const { name } = event.target;
-    this.setState({
-      showModal: true,
-      activetab: name,
-    });
-  }
+    setActivetab(name);
+    setShowModal(true);
+  };
 
-  onHide() {
-    this.setState((prevState) => {
-      return {
-        ...prevState,
-        showModal: false,
-      };
-    });
-  }
+  const onHide = () => {
+    setShowModal(false);
+  };
 
-  render() {
-    return (
-      <div>
-        <Navbar bg="light" variant="light" expand="md">
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-          </Nav>
-          {this.props.location.pathname === "/" && (
-            <Form inline>
-              <FormControl
-                type="text"
-                placeholder="Search"
-                className="mr-sm-2"
-              />
-              <Button variant="outline-primary">Search</Button>
-            </Form>
-          )}
-          <DropdownButton
-            alignRight
-            title="Account"
-            id="dropdown-menu-align-right"
-          >
-            <div className="container">
-              <Button variant="light" name="singIn" onClick={this.handleClick}>
-                Sign in
-              </Button>{" "}
-              <Button variant="dark" name="register" onClick={this.handleClick}>
-                Join
-              </Button>{" "}
-            </div>
-            <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-            <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
-          </DropdownButton>
-        </Navbar>
-        <Modal
-          show={this.state.showModal}
-          onHide={this.onHide}
-          size="lg"
-          aria-labelledby="contained-modal-title-vcenter"
-          centered
+  return (
+    <div>
+      <Navbar bg="light" variant="light" expand="md">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </Nav>
+        {props.location.pathname === "/" && (
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-primary">Search</Button>
+          </Form>
+        )}
+        <DropdownButton
+          alignRight
+          title="Account"
+          id="dropdown-menu-align-right"
         >
-          <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-vcenter">
-              Modal heading
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <h4>Studentko</h4>
-            <Tabs
-              defaultActiveKey={this.state.activetab}
-              id="uncontrolled-tab-example"
-            >
-              <Tab eventKey="register" title="Register">
-                <a href="/auth/google">Sign Up with Google</a>
-              </Tab>
-              <Tab eventKey="singIn" title="Sing in">
-                <a href="/auth/google">Sign In with Google</a>
-              </Tab>
-            </Tabs>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.onHide}>Close</Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
-    );
-  }
-}
+          <div className="container">
+            <Button variant="light" name="singIn" onClick={handleClick}>
+              Sign in
+            </Button>{" "}
+            <Button variant="dark" name="register" onClick={handleClick}>
+              Join
+            </Button>{" "}
+          </div>
+          <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+          <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+        </DropdownButton>
+      </Navbar>
+      <Modal
+        show={showModal}
+        onHide={onHide}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Modal heading
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Studentko</h4>
+          <Tabs defaultActiveKey={activetab} id="uncontrolled-tab-example">
+            <Tab eventKey="register" title="Register">
+              <a href="/auth/google">Sign Up with Google</a>
+            </Tab>
+            <Tab eventKey="singIn" title="Sing in">
+              <a href="/auth/google">Sign In with Google</a>
+            </Tab>
+          </Tabs>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
 
 export default withRouter(NavBar);
