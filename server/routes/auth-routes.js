a1376f2c1824231830ce595c09fe4dd053939dd6
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-09-02 15:12:37 +0200

    remove route tocheck if use is logged in

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -18,12 +18,5 @@ router.get(
   }
 );
 
-router.get("/isLoggedIn", (req, res) => {
-  if (req.isAuthenticated()) {
-    res.send("true");
-  } else {
-    res.send("false");
-  }
-});
 
 module.exports = router;
