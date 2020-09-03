8a5d3fb818e214f810031e40786fc43a118077ed
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-09-03 22:40:41 +0200

    remove useEffect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,21 +5,10 @@ import { UserContext } from "../context/UserContext";
 const Home = (props) => {
   const [isLoggedIn, setIsLoggedIn] = useContext(UserContext);
 
-  useEffect(() => {
-    console.log("useEffect is called");
-    // Get the parametar isLoggedIn value when components mounts
-    const query = new URLSearchParams(props.location.search);
-    const isLoggedInFromQueryString = query.get("isLoggedIn") === "true";
-    // Update context if used is logged in (if currentPage = localhost:5000/?isLoggedIn=true)
-    setIsLoggedIn((prevIsLoggedInt) => {
-      return isLoggedInFromQueryString;
-    });
-  }, []);
-
   return (
     <div>
       <Navbar />
-      This is the Homepage and isLoggedIn = {isLoggedIn.toString()}
+      This is the Homepage and isLoggedIn = {isLoggedIn}
     </div>
   );
 };
