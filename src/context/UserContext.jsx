58241a54393baf3931080b5e1b20976ecfd895d1
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-09-06 18:13:35 +0200

    set isLoggedIn state based on response from server

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,9 +1,19 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
+import axios from "axios";
 
 export const UserContext = createContext();
 
 export const UserProvider = (props) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    console.log("useEffect in UserContext called");
+    axios.get("http://localhost:5000/isLoggedIn").then((res) => {
+      const isLoggedInFromServer = res.data.isLoggedIn;
+      setIsLoggedIn(isLoggedInFromServer);
+    });
+  }, []);
+
   // Pass entire useState hook in as value prop
   return (
     <UserContext.Provider value={[isLoggedIn, setIsLoggedIn]}>
