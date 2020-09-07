0531d6fcae750595ac1a55f20e293117169e8a6e
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-09-07 11:22:54 +0200

    convert class to functional component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Navbar from "../components/NavBar";
 
-export default class About extends React.Component {
-  render() {
-    return (
-      <div>
-        <Navbar />
-        This is the About page
-      </div>
-    );
-  }
-}
+const About = () => {
+  return (
+    <div>
+      <Navbar />
+      This is the About page
+    </div>
+  );
+};
+
+export default About;
