30ba807054805256f53df735645f4fecf9b06903
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-09-16 15:41:16 +0200

    add "add reminder" button and popup modal to add reminder

--- src/pages/Reminders.jsx
+++ src/pages/Reminders.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import Navbar from "../components/NavBar";
+import { Button, Modal } from "react-bootstrap";
+import { useState } from "react";
 
 const Reminders = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div>
       <Navbar />
       This is reminders component
+      <br />
+      <Button variant="primary" onClick={handleShow}>
+        Add reminder
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add reminder</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Put the body in here</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
