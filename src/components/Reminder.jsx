318271d8486b4e11b94f355736645a8524fc8f66
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-09-26 19:24:54 +0200

    add reminder component

---
+++ src/components/Reminder.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Reminder = () => {
+  return (
+    <div style={{ backgroundColor: "#fca652", "height": "50px", "width": "100px" }}>
+      This is remidner
+    </div>
+  );
+};
+
+export default Reminder;
