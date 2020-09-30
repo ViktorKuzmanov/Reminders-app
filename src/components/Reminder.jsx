11505538bf905b955a618a73a4b56e9364946512
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-09-30 11:23:46 +0200

    show edit button on hover

--- src/components/Reminder.jsx
+++ src/components/Reminder.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col } from "react-bootstrap";
 
 const Reminder = (props) => {
+  const [showEditButton, setShowEditButton] = useState(false);
+
   const style = {
     backgroundColor: "#fca652",
     border: "1px solid",
     height: "50px",
     width: "100px",
   };
+
+  const styleButton = {
+    display: showEditButton ? "inline-block" : "none",
+  };
+
   return (
-    <Col md={3} style={style}>
+    <Col
+      onMouseEnter={(e) => setShowEditButton(true)}
+      onMouseLeave={(e) => setShowEditButton(false)}
+      md={3}
+      style={style}
+    >
       {props.name}
+      <button style={styleButton}>Edit</button>
     </Col>
   );
 };
